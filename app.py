703c0a8ac13e5249cb4ec98a632f047db3565b0d
Zura <70130285+papiashviliz@users.noreply.github.com>
2020-09-27 02:29:53 +0400

    config

--- app.py
+++ app.py
@@ -1,11 +1,51 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,send_file
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def homepage():
 
-    return render_template("index.html")
+    return render_template("homepage.html")
+
+@app.route('/about')
+def about():
+
+    return render_template("postGallery.html")
+
+@app.route('/templates/header.html')
+def header():
+
+    return send_file("templates/header.html")
+
+@app.route('/templates/slider.html')
+def slider():
+
+    return send_file("templates/slider.html")
+
+@app.route('/templates/postAbout.html')
+def post_about():
+
+    return send_file("templates/postAbout.html")
+
+@app.route('/templates/postGallery.html')
+def post_gallery():
+
+    return send_file("templates/postGallery.html")
+
+
+@app.route('/templates/postService.html')
+def post_service():
+
+    return send_file("templates/postService.html")
+
+
+
+
+
+
+
+
+
 
 
 if __name__ =='__main__':
