af02b8b9eb74812a7ea400f3791dc0ebc96046c0
Zura <70130285+papiashviliz@users.noreply.github.com>
2020-09-26 17:06:55 +0400

    configuration

--- app.py
+++ app.py
@@ -1,2 +1,10 @@
-from flask import Flask
+from flask import Flask,render_template
+from flask_restful import Api
+app = Flask(__name__)
+api = Api(app)
+@app.route('/')
+def index():
 
+    return render_template('index.html')
+
+app.run()
