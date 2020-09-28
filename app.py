27e9069f073e232c1e9eef8a767c08281111423f
Zura <70130285+papiashviliz@users.noreply.github.com>
2020-09-28 10:21:55 +0400

    app

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,send_file
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
@@ -6,39 +6,6 @@ app = Flask(__name__)
 def homepage():
 
     return render_template("homepage.html")
-
-@app.route('/about')
-def about():
-
-    return render_template("postGallery.html")
-
-@app.route('/templates/header.html')
-def header():
-
-    return send_file("templates/header.html")
-
-@app.route('/templates/slider.html')
-def slider():
-
-    return send_file("templates/slider.html")
-
-@app.route('/templates/postAbout.html')
-def post_about():
-
-    return send_file("templates/postAbout.html")
-
-@app.route('/templates/postGallery.html')
-def post_gallery():
-
-    return send_file("templates/postGallery.html")
-
-
-@app.route('/templates/postService.html')
-def post_service():
-
-    return send_file("templates/postService.html")
-
-
 
 
 
