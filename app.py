35da149031c8f258d7986a072210fa150405eeaa
Zura <70130285+papiashviliz@users.noreply.github.com>
2020-09-29 22:31:16 +0400

    mail

--- app.py
+++ app.py
@@ -1,47 +1,54 @@
-from flask import Flask,render_template,send_file
-
+from flask import Flask,render_template,request,flash
+from flask_mail import Mail, Message
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired,Email,EqualTo
+import smtplib
+from wtforms import ValidationError
 app = Flask(__name__)
+mail= Mail(app)
+app.config['SECRET_KEY'] = 'mysecretkey'
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'papiashvil@gmail.com'
+app.config['MAIL_PASSWORD'] = 'fsdqudsdyqcvxrch'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+mail = Mail(app)
+
+class ContactForm(FlaskForm):
+    name=StringField('სახელი', validators=[DataRequired()])
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    text = StringField('ტექსტი',validators=[DataRequired()])
+    submit = SubmitField('გაგზავნა')
+
+
 
 @app.route('/')
 def homepage():
 
-    return render_template("homepage.html")
-
-@app.route('/about')
-def about():
-
-    return render_template("postGallery.html")
-
-@app.route('/templates/header.html')
-def header():
-
-    return send_file("templates/header.html")
-
-@app.route('/templates/slider.html')
-def slider():
-
-    return send_file("templates/slider.html")
-
-@app.route('/templates/postAbout.html')
-def post_about():
-
-    return send_file("templates/postAbout.html")
-
-@app.route('/templates/postGallery.html')
-def post_gallery():
-
-    return send_file("templates/postGallery.html")
+    return render_template("homepage.html",check="home")
 
 
-@app.route('/templates/postService.html')
-def post_service():
-
-    return send_file("templates/postService.html")
+@app.route('/contact', methods=['GET', 'POST'])
+def send_mail():
+    form = ContactForm()
+    data = request.args
+    user_name = data.get("name")
+    user_mail = data.get("email")
+    user_text = data.get("text")
+    msg = Message(user_name, sender='papiashvil@gmail.com', recipients=['papiashvil@gmail.com',user_mail])
+    msg.body = user_name+" "+user_mail+" "+user_text
+    msg.title = 'title'
+    mail.send(msg)
+    flash('Blog Post Created')
 
 
 
 
 
+
+    return render_template('contact.html', form=form, check="contact")
 
 
 
